ae3fdb9d6da1769833cc485e3d76f46a166d16b9
K <K@host.docker.internal>
2022-10-17 00:41:13 +0900

    @Autowire 설정 해야되서 임시로 해둠

--- Heal/src/main/java/com/bc/heal/shop/service/ShopServiceImpl.java
+++ Heal/src/main/java/com/bc/heal/shop/service/ShopServiceImpl.java
@@ -3,17 +3,19 @@ package com.bc.heal.shop.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-
 import com.bc.heal.shop.mapper.ShopMapper;
 import com.bc.heal.vo.Shop;
 
+@Primary
 @Service
 public class ShopServiceImpl implements ShopService{
 	
 	@Autowired
 	private ShopMapper mapper;
 
+	
 	@Override
 	public void insert(List<Shop> list) {
 		if(!list.isEmpty()) {
